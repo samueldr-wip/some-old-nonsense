63d8471f4ed5226588ac8b795b0169c0f3bbe02a
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-06-25 23:15:00 -0400

    custom rootfs: Stop trying to load modules that aren't there.

--- pkgs/final-rootfs/default.nix
+++ pkgs/final-rootfs/default.nix
@@ -16,5 +16,6 @@ editSquashfs "miyoo-mini-final-rootfs" "${rootfs}/rootfs.img" {} ''
   patch -p1 < ${./0001-etc-profile-Drop-now-unneeded-mount-commands.patch}
   patch -p1 < ${./0002-misc-rework.patch}
   patch -p1 < ${./0001-main-somewhat-make-a-bit-more-user-friendly.patch}
+  patch -p1 < ${./0001-modules-Drop-modules-not-present-in-vendor-image.patch}
   )
 ''
