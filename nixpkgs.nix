28bdcacaf478e41a6436dabe1be72aefab5a30fc
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-07-01 00:43:46 -0400

    Pin Nixpkgs

---
+++ nixpkgs.nix
@@ -0,0 +1,4 @@
+import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/fcd48a5a0693f016a5c370460d0c2a8243b882dc.tar.gz";
+  sha256 = "sha256:06nyvac1azpa9gqdhj9py6ljsvbfgx49ilp8kf6w0w9clxba64vg";
+})
