fea569fbd5978ded3e1df08ed206ad5de12bce68
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-07-01 21:26:01 -0400

    busybox: Fix for FHS

--- pkgs/busybox/default.nix
+++ pkgs/busybox/default.nix
@@ -81,12 +81,10 @@ FHSBuilder {
 
   buildCommands = ''
     CFLAGS+=(
-      "-L${stdenv.cc.libc}/lib"
       "-L${gcc-unwrapped.lib}/lib"
       "-Os"
     )
     LDFLAGS+=(
-      "-L${stdenv.cc.libc}/lib"
       "-L${gcc-unwrapped.lib}/lib"
     )
     makeFlags=(
@@ -103,14 +101,12 @@ FHSBuilder {
     grep -v '${concatStringsSep ''\|'' (
         [
           "^CONFIG_CROSS_COMPILER_PREFIX="
-          "^CONFIG_PREFIX="
         ]
         ++ (map (name: ''CONFIG_${name}[= ]'') (enabled ++ disabled))
     )}' .config > tmp.config
     mv tmp.config .config
     cat <<EOF >> .config
     CONFIG_CROSS_COMPILER_PREFIX="${stdenv.cc.targetPrefix}"
-    CONFIG_PREFIX="$out"
     ${concatMapStringsSep "\n" (name: ''CONFIG_${name}=y'') enabled}
     ${concatMapStringsSep "\n" (name: ''# CONFIG_${name} is not set'') disabled}
     EOF
@@ -118,8 +114,10 @@ FHSBuilder {
     # Normalize config
     make oldconfig
 
-    # Build and install
-    make "''${makeFlags[@]}" install
+    # Build
+    make "''${makeFlags[@]}"
+    make "''${makeFlags[@]}" busybox.links
+    applets/install.sh "$out" --symlinks
 
     cp -v .config $config
   '';
